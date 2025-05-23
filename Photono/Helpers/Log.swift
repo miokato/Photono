acab2564bcbd89efe091597f7ae3ed600a0b9d39
miokato <miokato@me.com>
2025-05-24 06:48:57 +0900

    Add log function.

--- Photono/Helpers/Log.swift
+++ Photono/Helpers/Log.swift
@@ -5,3 +5,27 @@
 //  Created by mio kato on 2025/05/24.
 //
 
+import Foundation
+
+enum LogType: String {
+    case error = "ERROR"
+    case warning = "WARN"
+    case info = "INFO"
+    case debug = "DEBUG"
+}
+
+func log(
+    _ message: String,
+    with logType: LogType = .info,
+    error: Error? = nil,
+    file: String = #file,
+    function: String = #function,
+    line: Int = #line
+) {
+    let fileName = (file as NSString).lastPathComponent
+    let formattedMessage = "[[\(logType.rawValue)]] \(fileName):\(line) \(function) - \(message)"
+    
+    #if DEBUG
+    print(formattedMessage)
+    #endif
+}
